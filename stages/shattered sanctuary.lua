e3188bc2d118652b989ab5dbe4cb8cc8c388496c
OrangeSniper <51276538+OrangeSniper@users.noreply.github.com>
2021-10-30 08:26:05 -0400

    stage and charting

--- stages/shattered sanctuary.lua
+++ stages/shattered sanctuary.lua
@@ -4,11 +4,11 @@ function onCreate()
 	addLuaSprite('theWall',false)
 	setLuaSpriteScrollFactor('theWall',.8,.8)
 
-	makeLuaSprite('p1','sanctum/painting 1',102.75,57.2)
+	makeLuaSprite('p1','sanctum/painting 1',-200,50)
 	addLuaSprite('p1',false)
 	setLuaSpriteScrollFactor('p1',.8,.8)
 
-	makeLuaSprite('p2','sanctum/painting 2',1441.85,16)
+	makeLuaSprite('p2','sanctum/painting 2',700,0)
 	addLuaSprite('p2',false)
 	setLuaSpriteScrollFactor('p2',.8,.8)
 
@@ -16,11 +16,11 @@ function onCreate()
 	addLuaSprite('theFloor',false)
 	setLuaSpriteScrollFactor('theFloor',.9,.9)
 
-	makeLuaSprite('lamp1','sanctum/lamp',1579.05,261.55)
+	makeLuaSprite('lamp1','sanctum/lamp',-500,80)
 	addLuaSprite('lamp1',false)
 	setLuaSpriteScrollFactor('lamp1',.9,.9)
 
-	makeLuaSprite('lamp2','sanctum/lamp',270.95,253.1)
+	makeLuaSprite('lamp2','sanctum/lamp',1200,80)
 	addLuaSprite('lamp2',false)
 	setLuaSpriteScrollFactor('lamp2',.9,.9)
 
